af59eb7ce4c933020f7643bc5aa9e555f9681f74
Skip <skip@usecurling.com>
2026-06-23 19:18:47 -0300

    Refactor: Set Classic layout as default and remove selection

--- src/components/receipts/templates/ThirdPartyReceipt.tsx
+++ src/components/receipts/templates/ThirdPartyReceipt.tsx
@@ -1,11 +1,6 @@
 import { ReceiptTemplateProps } from '../types'
 import { ClassicReceipt } from './ClassicReceipt'
-import { ModernReceipt } from './ModernReceipt'
-import { MinimalistReceipt } from './MinimalistReceipt'
 
 export function ThirdPartyReceipt(props: ReceiptTemplateProps) {
-  const { data } = props
-  if (data.template === 'modern') return <ModernReceipt {...props} />
-  if (data.template === 'minimalist') return <MinimalistReceipt {...props} />
   return <ClassicReceipt {...props} />
 }
